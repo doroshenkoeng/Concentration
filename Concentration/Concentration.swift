a73435e31c17d8341bfcba750af083ead2435fc0
Sergey <doroshenkoeng@gmail.com>
2019-07-23 10:03:16 +0300

    Made indexOfOneAndOnlyFaceUpCard a computed variable and due to this simplified chooseCard method in the Concentration file

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -13,7 +13,26 @@ class Concentration {
     
     var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = index == newValue
+            }
+        }
+    }
     
     var score = 0
     
@@ -31,15 +50,8 @@ class Concentration {
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
                 // either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    if cards[flipDownIndex].isFaceUp {
-                        cards[flipDownIndex].isFaceUp = false
-                    }
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
