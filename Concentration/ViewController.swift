6288e901726b22a599c31e9ec69c3c92382f6106
Sergey Doroshenko <SDarashenka@gomel.iba.by>
2020-07-10 08:40:30 +0300

    Log ViewController

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad()")
     }
     
     private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
