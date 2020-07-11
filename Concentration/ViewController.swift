045e06c75c94339b4fd6bf5f17ff423069935742
Sergey Doroshenko <SDarashenka@gomel.iba.by>
2020-07-11 15:52:23 +0300

    Make a change in ViewController

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad()")
+        print("Log smth")
     }
     
     private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
